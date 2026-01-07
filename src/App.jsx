5b1e2339f4bc59d64d1bcf4eb1102ea2bcdc31c9
mysusi <quesada@mykiejan@gmail.com>
2026-01-07 09:38:10 +0800

    Initial React blog

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>My Blog</h1>
+      
+      <article>
+        <h2>What Is In Your Hands?</h2>
+        <p>
+          God uses simple things for powerful outcomes. No matter how small your resources are, God can use them.
+        </p>
+      </article>
+    </div>
+  );
+}
+
+export default App;
